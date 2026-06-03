f34a2b615c39bcb008b6fb850e26b9310f1adacb
Your Name <anuskamagarrr@gmail.com>
2026-06-03 19:01:45 +0545

    updated profile picture

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion"
-import profileImg from "../assets/profile.jpg"
+import profileImg from "../assets/profile.jpeg"
 
 export default function Hero() {
   const scrollToProjects = () => {
